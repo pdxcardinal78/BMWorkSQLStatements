99984b17c60bb9d72ec9c2a6fd0f4eb4d0b74c00
Ryan Johnson <ryanj.johnson@gmail.com>
2018-12-06 10:56:06 -0800

    Updating NC Count file

--- Count NCs by Status by OriginCat.sql
+++ Count NCs by Status by OriginCat.sql
@@ -1,18 +1,25 @@
 Select Status, Count(NCR) as CurrentStatus
 From uniPoint_CompanyH.dbo.PT_NC
-Where NCR_Date between '8/5/18' and '9/1/18'
+Where NCR_Date between '11/5/18' and '12/1/18'
 Group By Status
 Order By Count(NCR) DESC
 
 
 Select Origin_category, Count(NCR) as Count
 From uniPoint_CompanyH.dbo.PT_NC
-Where NCR_Date between '8/5/18' and '9/1/18' --and NC_type = 'Vendor'
+Where NCR_Date between '11/5/18' and '12/1/18' and Status NOT IN ('Verification', 'Origination')
 Group By Origin_category
 Order By Count(NCR) DESC
 
 Select NC_type, Count(NCR) as Count
 From uniPoint_CompanyH.dbo.PT_NC
-Where NCR_Date between '8/5/18' and '9/1/18'
+Where NCR_Date between '11/5/18' and '12/1/18'
 Group By NC_type
 Order By Count(NCR) DESC
+
+
+Select MONTH(NCR_Date) as Month, COUNT(NCR) as Count, (MONTH(NCR_Date) - MONTH(getdate())) as OrderRank
+FROM uniPoint_CompanyH.dbo.PT_NC
+WHERE NCR_Date > DATEADD(mm, -13, getdate())
+Group By MONTH(NCR_Date)
+Order By OrderRank DESC
